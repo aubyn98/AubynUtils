a7cb88485eacc98d6f04d2a3ea18e08888e4a195
aubyn <aubyn@vip.qq.com>
2023-11-18 15:06:47 +0800

    change d.ts

--- lib/storage/index.d.ts
+++ lib/storage/index.d.ts
@@ -1,11 +1,14 @@
 export class Storage {
-  constructor(config: { type: 'localStorage' | 'sessionStorage'; prefix?: string; expire?: number })
-  setItem(key: string, val: any, opts?: { expire?: number; renewal?: boolean }): void
-  getItem(key: string, defaultVal: any): any
-  removeItem(key: string): string
-  autoAddPrefix(key: string): string
-  autoRemovePrefix(key: string): string
+  constructor(config: { type: 'localStorage' | 'sessionStorage'; prefix?: string; expire?: number });
+  setItem(key: string, val: any, opts?: { expire?: number; renewal?: boolean }): void;
+  getItem<T>(key: string, defaultVal?: T): T;
+  removeItem(key: string): void;
+  setItem_simple(key: string, val: any): void;
+  getItem_simple<T>(key: string, defaultVal?: T): T;
+  removeItem_simple(key: string): void;
+  autoAddPrefix(key: string): string;
+  autoRemovePrefix(key: string): string;
 }
 
-export const local: InstanceType<typeof Storage>
-export const session: InstanceType<typeof Storage>
+export const local: InstanceType<typeof Storage>;
+export const session: InstanceType<typeof Storage>;
