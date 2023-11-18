9da2ff2d7ec8d1c40489efd9b89bc82d139672a0
aubyn <aubyn@vip.qq.com>
2023-11-18 14:46:16 +0800

    change storage

--- lib/storage/index.d.ts
+++ lib/storage/index.d.ts
@@ -1,8 +1,8 @@
 export class Storage {
   constructor(config: { type: 'localStorage' | 'sessionStorage'; prefix?: string; expire?: number })
-  setStorage(key: string, val: any, opts?: { expire?: number; renewal?: boolean }): void
-  getStorage(key: string, defaultVal: any): any
-  removeStorage(key: string): string
+  setItem(key: string, val: any, opts?: { expire?: number; renewal?: boolean }): void
+  getItem(key: string, defaultVal: any): any
+  removeItem(key: string): string
   autoAddPrefix(key: string): string
   autoRemovePrefix(key: string): string
 }
